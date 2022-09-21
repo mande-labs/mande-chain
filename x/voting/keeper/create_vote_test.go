1d52ba89f16a253c85b76a065625237d2998b294
Samarth Tambad <tambad.samarth@gmail.com>
2022-09-20 19:15:11 -0500

    add additional test cases

--- x/voting/keeper/create_vote_test.go
+++ x/voting/keeper/create_vote_test.go
@@ -189,6 +189,134 @@ func (suite *VotingKeeperTestSuite) TestCreateVote_UnCast_Negative_Valid() {
 	suite.validateVoteBook(voteBookEntry, 0, 50)
 }
 
+func (suite *VotingKeeperTestSuite) TestValidation_VoteExceedsBalance() {
+	app, ctx, msgServer := suite.App, sdk.WrapSDKContext(suite.Ctx), suite.MsgServer
+	balances := sdk.NewCoins(testutil.NewMandCoin(500))
+
+	addr1 := sdk.AccAddress("addr1_______________")
+	acc1 := app.AccountKeeper.NewAccountWithAddress(suite.Ctx, addr1)
+	app.AccountKeeper.SetAccount(suite.Ctx, acc1)
+	suite.Require().NoError(testutil.FundAccount(app.BankKeeper, suite.Ctx, addr1, balances))
+
+	validator := app.StakingKeeper.GetAllValidators(suite.Ctx)[0]
+	valAddr, _ := sdk.ValAddressFromBech32(validator.OperatorAddress)
+	addr2, _ := sdk.AccAddressFromHex(hex.EncodeToString(valAddr.Bytes()))
+
+	acc1Balance := app.BankKeeper.GetBalance(suite.Ctx, addr1, "mand").Amount.Uint64()
+	suite.Require().Equal(uint64(500), acc1Balance)
+
+	// ============= addr1 votes addr2 ===============
+	msgCreateVote := &types.MsgCreateVote{
+		Creator:  addr1.String(),
+		Receiver: addr2.String(),
+		Count:    600,
+		Mode:     1, // cast
+	}
+	_, err := msgServer.CreateVote(ctx, msgCreateVote)
+	suite.Require().ErrorIs(err, types.ErrNotEnoughMand)
+}
+
+func (suite *VotingKeeperTestSuite) TestValidation_NoVotingRecord() {
+	app, ctx, msgServer := suite.App, sdk.WrapSDKContext(suite.Ctx), suite.MsgServer
+	balances := sdk.NewCoins(testutil.NewMandCoin(500))
+
+	addr1 := sdk.AccAddress("addr1_______________")
+	acc1 := app.AccountKeeper.NewAccountWithAddress(suite.Ctx, addr1)
+	app.AccountKeeper.SetAccount(suite.Ctx, acc1)
+	suite.Require().NoError(testutil.FundAccount(app.BankKeeper, suite.Ctx, addr1, balances))
+
+	validator := app.StakingKeeper.GetAllValidators(suite.Ctx)[0]
+	valAddr, _ := sdk.ValAddressFromBech32(validator.OperatorAddress)
+	addr2, _ := sdk.AccAddressFromHex(hex.EncodeToString(valAddr.Bytes()))
+
+	acc1Balance := app.BankKeeper.GetBalance(suite.Ctx, addr1, "mand").Amount.Uint64()
+	suite.Require().Equal(uint64(500), acc1Balance)
+
+	// ============= addr1 votes addr2 ===============
+	msgCreateVote := &types.MsgCreateVote{
+		Creator:  addr1.String(),
+		Receiver: addr2.String(),
+		Count:    100,
+		Mode:     0, // un-cast
+	}
+	_, err := msgServer.CreateVote(ctx, msgCreateVote)
+	suite.Require().ErrorIs(err, types.ErrNoVoteRecord)
+}
+
+func (suite *VotingKeeperTestSuite) TestValidation_PositiveVotesCastedLimit() {
+	app, ctx, msgServer := suite.App, sdk.WrapSDKContext(suite.Ctx), suite.MsgServer
+	balances := sdk.NewCoins(testutil.NewMandCoin(500))
+
+	addr1 := sdk.AccAddress("addr1_______________")
+	acc1 := app.AccountKeeper.NewAccountWithAddress(suite.Ctx, addr1)
+	app.AccountKeeper.SetAccount(suite.Ctx, acc1)
+	suite.Require().NoError(testutil.FundAccount(app.BankKeeper, suite.Ctx, addr1, balances))
+
+	validator := app.StakingKeeper.GetAllValidators(suite.Ctx)[0]
+	valAddr, _ := sdk.ValAddressFromBech32(validator.OperatorAddress)
+	addr2, _ := sdk.AccAddressFromHex(hex.EncodeToString(valAddr.Bytes()))
+
+	acc1Balance := app.BankKeeper.GetBalance(suite.Ctx, addr1, "mand").Amount.Uint64()
+	suite.Require().Equal(uint64(500), acc1Balance)
+
+	// ============= addr1 votes addr2 ===============
+	msgCreateVote := &types.MsgCreateVote{
+		Creator:  addr1.String(),
+		Receiver: addr2.String(),
+		Count:    100,
+		Mode:     1, // cast
+	}
+	_, err := msgServer.CreateVote(ctx, msgCreateVote)
+	suite.Require().NoError(err)
+
+	// ============= addr1 un-votes addr2 ===============
+	msgCreateVote = &types.MsgCreateVote{
+		Creator:  addr1.String(),
+		Receiver: addr2.String(),
+		Count:    200,
+		Mode:     0, // un-cast
+	}
+	_, err = msgServer.CreateVote(ctx, msgCreateVote)
+	suite.Require().ErrorIs(err, types.ErrPositiveVotesCastedLimit)
+}
+
+func (suite *VotingKeeperTestSuite) TestValidation_NegativeVotesCastedLimit() {
+	app, ctx, msgServer := suite.App, sdk.WrapSDKContext(suite.Ctx), suite.MsgServer
+	balances := sdk.NewCoins(testutil.NewMandCoin(500))
+
+	addr1 := sdk.AccAddress("addr1_______________")
+	acc1 := app.AccountKeeper.NewAccountWithAddress(suite.Ctx, addr1)
+	app.AccountKeeper.SetAccount(suite.Ctx, acc1)
+	suite.Require().NoError(testutil.FundAccount(app.BankKeeper, suite.Ctx, addr1, balances))
+
+	validator := app.StakingKeeper.GetAllValidators(suite.Ctx)[0]
+	valAddr, _ := sdk.ValAddressFromBech32(validator.OperatorAddress)
+	addr2, _ := sdk.AccAddressFromHex(hex.EncodeToString(valAddr.Bytes()))
+
+	acc1Balance := app.BankKeeper.GetBalance(suite.Ctx, addr1, "mand").Amount.Uint64()
+	suite.Require().Equal(uint64(500), acc1Balance)
+
+	// ============= addr1 votes addr2 ===============
+	msgCreateVote := &types.MsgCreateVote{
+		Creator:  addr1.String(),
+		Receiver: addr2.String(),
+		Count:    -100,
+		Mode:     1, // cast
+	}
+	_, err := msgServer.CreateVote(ctx, msgCreateVote)
+	suite.Require().NoError(err)
+
+	// ============= addr1 un-votes addr2 ===============
+	msgCreateVote = &types.MsgCreateVote{
+		Creator:  addr1.String(),
+		Receiver: addr2.String(),
+		Count:    -200,
+		Mode:     0, // un-cast
+	}
+	_, err = msgServer.CreateVote(ctx, msgCreateVote)
+	suite.Require().ErrorIs(err, types.ErrNegativeVotesCastedLimit)
+}
+
 func (suite *VotingKeeperTestSuite) validateAggregateVoteCount(aggregateVoteCount types.AggregateVoteCount, casted uint64, positiveReceived uint64, negativeReceived uint64) {
 	suite.Require().Equal(casted, aggregateVoteCount.GetCasted())
 	suite.Require().Equal(positiveReceived, aggregateVoteCount.GetPositiveReceived())

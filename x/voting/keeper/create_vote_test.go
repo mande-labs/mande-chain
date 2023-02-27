b483b65280d0fe42483a8e92bf2cbce333159549
Pratik <prtkagarwal15@gmail.com>
2023-02-27 12:03:20 +0530

    X related unit tests

--- x/voting/keeper/create_vote_test.go
+++ x/voting/keeper/create_vote_test.go
@@ -10,7 +10,6 @@ import (
 	votingtypes "mande-chain/x/voting/types"
 	oracletypes "mande-chain/x/oracle/types"
 	"testing"
-	"fmt"
 )
 
 type VotingKeeperTestSuite struct {
@@ -574,6 +573,136 @@ func (suite *VotingKeeperTestSuite) TestValidation_NoVotingRecord() {
 	}
 	_, err := msgServer.CreateVote(ctx, msgCreateVote)
 	suite.Require().ErrorIs(err, types.ErrNoVoteRecord)
+}
+
+func (suite *VotingKeeperTestSuite) Test_X_Value_Change_Non_Validator() {
+	app, ctx, msgServer, keeper := suite.App, sdk.WrapSDKContext(suite.Ctx), suite.MsgServer, suite.App.VotingKeeper
+	balances := sdk.NewCoins(testutil.NewMandCoin(10000))
+
+	addr1 := sdk.AccAddress("addr1_______________")
+	acc1 := app.AccountKeeper.NewAccountWithAddress(suite.Ctx, addr1)
+	app.AccountKeeper.SetAccount(suite.Ctx, acc1)
+	suite.Require().NoError(testutil.FundAccount(app.BankKeeper, suite.Ctx, addr1, balances))
+
+	addr2 := sdk.AccAddress("addr2_______________")
+	acc2 := app.AccountKeeper.NewAccountWithAddress(suite.Ctx, addr2)
+	app.AccountKeeper.SetAccount(suite.Ctx, acc2)
+
+	app.OracleKeeper.SetNetworkConstantResult(suite.Ctx, oracletypes.OracleRequestID(1), oracletypes.NetworkConstantResult{Response: "1.0"})
+	app.OracleKeeper.SetLastNetworkConstantID(suite.Ctx, oracletypes.OracleRequestID(1))
+
+	// ============= addr1 votes addr2 ===============
+	msgCreateVote := &types.MsgCreateVote{
+		Creator:  addr1.String(),
+		Receiver: addr2.String(),
+		Count:    100,
+		Mode:     1, // cast
+	}
+	_, err := msgServer.CreateVote(ctx, msgCreateVote)
+	suite.Require().NoError(err)
+
+	// validate aggregate vote counts after tx
+	aggregateVoteCountCreator, _ := keeper.GetAggregateVotesCasted(suite.Ctx, addr1.String())
+	aggregateVoteCountReceiver, _ := keeper.GetAggregateVotesReceived(suite.Ctx, addr2.String())
+	suite.Require().Equal(uint64(100), aggregateVoteCountCreator.GetPositive())
+	suite.Require().Equal(uint64(100), aggregateVoteCountReceiver.GetPositive())
+
+	// validate vote book
+	voteBookEntry, _ := keeper.GetVoteBook(suite.Ctx, types.VoteBookIndex(addr2.String(), addr1.String()))
+	suite.validateVoteBook(voteBookEntry, 100, 0)
+
+	credibility, _ := keeper.GetCredibility(suite.Ctx, addr2.String())
+	suite.Require().Equal("100.00", credibility.Score) // because X is set to 1
+
+	app.OracleKeeper.SetNetworkConstantResult(suite.Ctx, oracletypes.OracleRequestID(2), oracletypes.NetworkConstantResult{Response: "0.5"})
+	app.OracleKeeper.SetLastNetworkConstantID(suite.Ctx, oracletypes.OracleRequestID(2))
+
+	keeper.UpdateValidatorsCredibility(suite.Ctx)
+
+	credibility, _ = keeper.GetCredibility(suite.Ctx, addr2.String())
+	suite.Require().Equal("100.00", credibility.Score) // because X is set to 1
+
+	// ============= addr1 votes addr2 ===============
+	msgCreateVote = &types.MsgCreateVote{
+		Creator:  addr1.String(),
+		Receiver: addr2.String(),
+		Count:    100,
+		Mode:     1, // cast
+	}
+	_, err = msgServer.CreateVote(ctx, msgCreateVote)
+	suite.Require().NoError(err)
+
+	// validate aggregate vote counts after tx
+	aggregateVoteCountCreator, _ = keeper.GetAggregateVotesCasted(suite.Ctx, addr1.String())
+	aggregateVoteCountReceiver, _ = keeper.GetAggregateVotesReceived(suite.Ctx, addr2.String())
+	suite.Require().Equal(uint64(200), aggregateVoteCountCreator.GetPositive())
+	suite.Require().Equal(uint64(200), aggregateVoteCountReceiver.GetPositive())
+
+	// validate vote book
+	voteBookEntry, _ = keeper.GetVoteBook(suite.Ctx, types.VoteBookIndex(addr2.String(), addr1.String()))
+	suite.validateVoteBook(voteBookEntry, 200, 0)
+
+	credibility, _ = keeper.GetCredibility(suite.Ctx, addr2.String())
+	suite.Require().Equal("14.14", credibility.Score) // because X is set to 1
+}
+
+func (suite *VotingKeeperTestSuite) Test_X_Value_Change_Validator() {
+	app, ctx, msgServer, keeper := suite.App, sdk.WrapSDKContext(suite.Ctx), suite.MsgServer, suite.App.VotingKeeper
+	balances := sdk.NewCoins(testutil.NewMandCoin(10000))
+
+	addr1 := sdk.AccAddress("addr1_______________")
+	acc1 := app.AccountKeeper.NewAccountWithAddress(suite.Ctx, addr1)
+	app.AccountKeeper.SetAccount(suite.Ctx, acc1)
+	suite.Require().NoError(testutil.FundAccount(app.BankKeeper, suite.Ctx, addr1, balances))
+
+	validator := app.StakingKeeper.GetAllValidators(suite.Ctx)[0]
+	valAddr, _ := sdk.ValAddressFromBech32(validator.OperatorAddress)
+	addr2, _ := sdk.AccAddressFromHex(hex.EncodeToString(valAddr.Bytes()))
+
+	app.OracleKeeper.SetNetworkConstantResult(suite.Ctx, oracletypes.OracleRequestID(1), oracletypes.NetworkConstantResult{Response: "1.0"})
+	app.OracleKeeper.SetLastNetworkConstantID(suite.Ctx, oracletypes.OracleRequestID(1))
+
+	// ============= addr1 votes addr2 ===============
+	msgCreateVote := &types.MsgCreateVote{
+		Creator:  addr1.String(),
+		Receiver: addr2.String(),
+		Count:    100,
+		Mode:     1, // cast
+	}
+	_, err := msgServer.CreateVote(ctx, msgCreateVote)
+	suite.Require().NoError(err)
+
+	// validate aggregate vote counts after tx
+	aggregateVoteCountCreator, _ := keeper.GetAggregateVotesCasted(suite.Ctx, addr1.String())
+	aggregateVoteCountReceiver, _ := keeper.GetAggregateVotesReceived(suite.Ctx, addr2.String())
+	suite.Require().Equal(uint64(100), aggregateVoteCountCreator.GetPositive())
+	suite.Require().Equal(uint64(100), aggregateVoteCountReceiver.GetPositive())
+
+	// validate vote book
+	voteBookEntry, _ := keeper.GetVoteBook(suite.Ctx, types.VoteBookIndex(addr2.String(), addr1.String()))
+	suite.validateVoteBook(voteBookEntry, 100, 0)
+
+	credibility, _ := keeper.GetCredibility(suite.Ctx, addr2.String())
+	suite.Require().Equal("100.00", credibility.Score) // because X is set to 1
+
+	app.OracleKeeper.SetNetworkConstantResult(suite.Ctx, oracletypes.OracleRequestID(2), oracletypes.NetworkConstantResult{Response: "0.5"})
+	app.OracleKeeper.SetLastNetworkConstantID(suite.Ctx, oracletypes.OracleRequestID(2))
+
+	credibility, _ = keeper.GetCredibility(suite.Ctx, addr2.String())
+	suite.Require().Equal("100.00", credibility.Score) // because X is set to 1
+
+	keeper.UpdateValidatorsCredibility(suite.Ctx)
+
+	credibility, _ = keeper.GetCredibility(suite.Ctx, addr2.String())
+	suite.Require().Equal("10.00", credibility.Score) // because X is set to 1
+
+	app.OracleKeeper.SetNetworkConstantResult(suite.Ctx, oracletypes.OracleRequestID(3), oracletypes.NetworkConstantResult{Response: "0.1"})
+	app.OracleKeeper.SetLastNetworkConstantID(suite.Ctx, oracletypes.OracleRequestID(3))
+
+	keeper.UpdateValidatorsCredibility(suite.Ctx)
+
+	credibility, _ = keeper.GetCredibility(suite.Ctx, addr2.String())
+	suite.Require().Equal("1.58", credibility.Score) // because X is set to 1
 }
 
 func (suite *VotingKeeperTestSuite) validateVoteBook(voteBookEntry types.VoteBook, positive uint64, negative uint64) {

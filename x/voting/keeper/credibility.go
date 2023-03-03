a3946f87438abda44d3ab8165e821aa55ce4cc57
Pratik <prtkagarwal15@gmail.com>
2023-03-03 08:18:54 +0530

    cred exponent fix

--- x/voting/keeper/credibility.go
+++ x/voting/keeper/credibility.go
@@ -90,11 +90,8 @@ func (k Keeper) GetAllCredibility(ctx sdk.Context) (list []types.Credibility) {
 }
 
 func (k Keeper) UpdateCredibility(ctx sdk.Context, receiver string, credibility *types.Credibility) {
-	ctx.Logger().Info(fmt.Sprintf("entered UpdateCredibility"))
 	lastReqId := k.oracleKeeper.GetLastNetworkConstantID(ctx)
-	ctx.Logger().Info(fmt.Sprintf("check lastReqId here: %d", lastReqId))
 	xResult, _ := k.oracleKeeper.GetNetworkConstantResult(ctx, oracletypes.OracleRequestID(lastReqId))
-	ctx.Logger().Info(fmt.Sprintf("check xResult here: %s", xResult.Response))
 
 	x, _ := strconv.ParseFloat(xResult.Response, 64)
 	var credScore float64
@@ -102,37 +99,24 @@ func (k Keeper) UpdateCredibility(ctx sdk.Context, receiver string, credibility 
 	// TODO: Implement some caching mechanism to cache cred scores for votes and only compute for the current vote
 	iterator := k.GetVoteBookIteratorByPrefix(ctx, receiver)
 	defer iterator.Close()
-	ctx.Logger().Info(fmt.Sprintf("iteraction started"))
 	for ; iterator.Valid(); iterator.Next() {
 		var voteBook types.VoteBook
 		err := k.UnmarshalVoteBook(iterator.Value(), &voteBook)
 		if err != nil {
 			panic(err)
 		}
-		ctx.Logger().Info(fmt.Sprintf("computing x for votebook idx: %s", voteBook.Index))
 		ballot := int64(voteBook.Positive - voteBook.Negative)
-		ctx.Logger().Info(fmt.Sprintf("ballot count: %d", ballot))
 		voteCount := float64(intAbs2(ballot))
-		ctx.Logger().Info(fmt.Sprintf("vote count: %f", voteCount))
 		if ballot < 0 {
 			credScore = credScore + math.Pow(voteCount, x)*-1.0
 		} else {
 			credScore = credScore + math.Pow(voteCount, x)
 		}
-
-		ctx.Logger().Info(fmt.Sprintf("cred score after computation: %.2f", credScore))
 	}
-	ctx.Logger().Info(fmt.Sprintf("iteraction ended"))
-
-	ctx.Logger().Info(fmt.Sprintf("final cred score after computation: %.2f", credScore))
-	credibility.Score = fmt.Sprintf("%.2f", credScore)
+	credibility.Score = fmt.Sprintf("%.2f", credScore * (math.Pow(1000000,(1-x))))
 	credibility.ForX = xResult.Response
 
 	k.SetCredibility(ctx, *credibility)
 
-	ctx.Logger().Info(fmt.Sprintf("check x here: %s", xResult))
-	ctx.Logger().Info(fmt.Sprintf("check xDec here: %.2f", x))
-	ctx.Logger().Info(fmt.Sprintf("check score here: %s", credibility.Score))
-	ctx.Logger().Info(fmt.Sprintf("exiting UpdateCredibility"))
 	return
 }
